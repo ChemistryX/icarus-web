490e35fad1350c1e8e980d523cbbe70a58e2f510
Suhan Ha <starpiung@naver.com>
2022-02-22 04:33:04 +0900

    devise localization, tailwind support

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  attr_readonly :email
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
